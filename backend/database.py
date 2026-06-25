8fd8ae7a5119fb39b3ea0d19935180cd85bb20dc
DanielaVenturi <danielaventuri14@gmail.com>
2026-06-25 12:36:01 -0300

    Criação da tabela alunos

---
+++ backend/database.py
@@ -0,0 +1,33 @@
+import sqlite3
+
+def conectar():
+    return sqlite3.connect("banco.db")
+
+def criar_tabelas():
+
+    conn = conectar()
+
+    cursor = conn.cursor()
+
+    cursor.execute("""
+        CREATE TABLE IF NOT EXISTS alunos(
+            matricula INTEGER PRIMARY KEY,
+            nome TEXT NOT NULL,
+            curso TEXT NOT NULL
+        )
+    """)
+
+    cursor.execute("""
+        CREATE TABLE IF NOT EXISTS notas(
+            id INTEGER PRIMARY KEY AUTOINCREMENT,
+            aluno_matricula INTEGER,
+            nota1 REAL,
+            nota2 REAL,
+            nota3 REAL,
+            FOREIGN KEY(aluno_matricula)
+                REFERENCES alunos(matricula)
+        )
+    """)
+
+    conn.commit()
+    conn.close()
